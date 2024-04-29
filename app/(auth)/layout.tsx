4801a35ad2db30bd2c1454a87055879a9507d44b
Nazar Tymiv <nazartumiv@gmail.com>
2024-04-30 00:28:27 +0100

    feat: add clerk authentication and install prisma

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="flex min-h-screen items-center justify-center">
       {children}
     </div>
   )
