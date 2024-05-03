983e8e10601b91968fded8f27a9440e9cf971a64
Nazar Tymiv <nazartumiv@gmail.com>
2024-05-04 00:06:13 +0100

    feat: add navigation sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServerPage = () => {
+
+
+  return (
+    <div>ServerPage</div>
+  )
+}
+
+export default ServerPage
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -19,7 +19,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image fill sizes="50px" src={value} alt="Upload" className="rounded-full object-cover" />
+        <Image fill sizes="20" priority src={value} alt="Upload" className="rounded-full object-cover" />
         <button onClick={() => onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
           <X className="h-4 w-4" />
         </button>
